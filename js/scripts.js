57d35a1f9d76059b4c7eb2cb258be589b4544953
jrivera-technisys <jrivera@technisys.com>
2017-01-14 18:35:02 -0300

    first

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function init(){
+   Parse.initialize("itnJ9pflt3HxJgjCHEaG6NEu6ABqwnww54lnx0Td","mnu1d9X548aECXlTol3VQ6thgn4OSkYaY1MyJBMY");
+}
+
+function clear(inputid){
+   $(inputid).val("");
+}
+
